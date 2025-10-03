a8c2542625aaec1452823ff60f378bc659d529e0
Sgtryan10 <sgtryan123456@gmail.com>
2025-10-03 00:42:20 -0400

    Added random paddle NPC movement

--- Assets/Scripts/bowlingGame/PaddleSpawner.cs
+++ Assets/Scripts/bowlingGame/PaddleSpawner.cs
@@ -1,27 +1,89 @@
 using UnityEngine;
+using System.Collections;
 
-public class PaddleSpawner : MonoBehaviour
+public class paddleSpawner : MonoBehaviour
 {
     public GameObject paddle;
     public int numberOfPaddles;
-    public float spawnRangeX;
-    public float spawnRangeZ;
     public float padHeightY;
+    public float moveSpeed;
+    public float forceApplicationInterval;
+    public float boundaryX;
+    public float boundaryZ;
 
     void Start()
     {
-        SpawnPads();
-    }
-
-    void SpawnPads()
-    {
         for (int i = 0; i < numberOfPaddles; i++)
         {
-            float randomX = Random.Range(-spawnRangeX, spawnRangeX);
-            float randomZ = Random.Range(-spawnRangeZ, spawnRangeZ);
+            float randomX = Random.Range(-boundaryX, boundaryX);
+            float randomZ = Random.Range(-boundaryZ, boundaryZ);
 
             Vector3 spawnPosition = new Vector3(randomX, padHeightY, randomZ);
-            Instantiate(paddle, spawnPosition, Quaternion.identity);
+            GameObject newPaddle = Instantiate(paddle, spawnPosition, Quaternion.identity);
+
+            StartCoroutine(MovePaddleRandomly(newPaddle));
+        }
+    }
+
+    IEnumerator MovePaddleRandomly(GameObject p)
+    {
+        Rigidbody rb = p.GetComponent<Rigidbody>();
+
+        while(true)
+        {
+            if(p == null) {
+                yield break;
+            }
+
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            float randomX = Random.Range(-1f, 1f);
+            float randomZ = Random.Range(-1f, 1f);
+            Vector3 randomDirection = new Vector3(randomX, 0, randomZ).normalized;
+
+            rb.AddForce(randomDirection * moveSpeed, ForceMode.VelocityChange);
+
+            yield return new WaitForSeconds(forceApplicationInterval);
+
+            while (rb.velocity.magnitude > 0)
+            {
+                Vector3 currentPos = p.transform.position;
+                bool needsCorrection = false;
+
+                if (currentPos.x > boundaryX)
+                {
+                    currentPos.x = boundaryX;
+                    rb.velocity = new Vector3(-Mathf.Abs(rb.velocity.x), 0, rb.velocity.z);
+                    needsCorrection = true;
+                }
+                else if (currentPos.x < -boundaryX)
+                {
+                    currentPos.x = -boundaryX;
+                    rb.velocity = new Vector3(Mathf.Abs(rb.velocity.x), 0, rb.velocity.z);
+                    needsCorrection = true;
+                }
+
+                if (currentPos.z > boundaryZ)
+                {
+                    currentPos.z = boundaryZ;
+                    rb.velocity = new Vector3(rb.velocity.x, 0, -Mathf.Abs(rb.velocity.z));
+                    needsCorrection = true;
+                }
+                else if (currentPos.z < -boundaryZ)
+                {
+                    currentPos.z = -boundaryZ;
+                    rb.velocity = new Vector3(rb.velocity.x, 0, Mathf.Abs(rb.velocity.z));
+                    needsCorrection = true;
+                }
+
+                if (needsCorrection)
+                {
+                    p.transform.position = new Vector3(currentPos.x, padHeightY, currentPos.z);
+                }
+
+                yield return new WaitForFixedUpdate();
+            }
         }
     }
 }
